531219018131420dec85d470c4e66b6a8c7a81f8
Bruno González <brunobrunito048@gmail.com>
2026-05-22 21:56:58 -0300

    Add files via upload

--- trivia-app/js/TriviaAPI.js
+++ trivia-app/js/TriviaAPI.js
@@ -4,19 +4,40 @@ class TriviaAPI {
     constructor() {
     }
     async getPreguntas(cantidad = 10, categoria = "", dificultad = "") {
+        const endpoint = `https://opentdb.com/api.php?amount=${cantidad}&category=${categoria}&difficulty=${dificultad}&type=multiple`;
+        
+        try {
+            const response = await fetch(endpoint);
 
-        const endpoint = "https://opentdb.com/api.php?amount=10&type=multiple";
-        const response = await fetch(endpoint);
-        const data = await response.json();
-        return data.results;
+            if(!response.ok) {
+                throw new Error(`Error ${response.status}: ${response.statusText}`);
+            }
+
+            const data = await response.json();
+            return data.results;
+
+        } catch (error) {
+            console.error('Falló la petición:', error.message);
+        }
 
     }
 
     async getCategorias() {
         const endpoint = "https://opentdb.com/api_category.php";
-        const response = await fetch(endpoint);
-        const data = await response.json();
-        return data.trivia_categories;
+        
+        try {
+            const response = await fetch(endpoint);
+
+            if(!response.ok) {
+                throw new Error(`Error ${response.status}: ${response.statusText}`);
+            }
+
+            const data = await response.json();
+            return data.trivia_categories;
+
+        } catch (error) {
+            console.error('Falló la petición:', error.message);
+        }
     }
 
 }
